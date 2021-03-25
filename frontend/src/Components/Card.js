9ecb92acc176471d365107e71552fe2ad1f48586
HyrvinCoulon <bangolamoukogho@gmail.com>
2021-03-25 12:14:33 -0800

    .

--- frontend/src/Components/Card.js
+++ frontend/src/Components/Card.js
@@ -5,7 +5,7 @@ class Cards extends React.Component{
 
     render(){
         return(
-            <Card>
+            <Card style={{margin:"10px 0", padding:"5px"}}>
                 <Card.Title>{this.props.user.name}</Card.Title>
                 <Card.Text>This is one of the user.</Card.Text>
             </Card>
