236e7b218194cb2b2082b0f56aeb96048ec11afd
HyrvinCoulon <bangolamoukogho@gmail.com>
2021-04-07 06:18:16 -0800

    .

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,21 @@
+const path = require("path")
+
 module.exports = {
     module: {
+      /*entry: 'index.js',
+      output: {
+        path: path.resolve(__dirname + '/'),
+        filename: 'bundle.js',
+        publicPath: '/'
+      }, */
+      /*entry: {
+        frontend: './src/index.js',
+      },
+      output: {
+        path: path.resolve(__dirname, '/static/frontend'),
+        filename: './main.js',
+        
+      },*/
       rules: [
         {
           test: /\.js$/,
